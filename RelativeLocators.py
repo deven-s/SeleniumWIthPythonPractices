1b3ae9c1a275f776f9a245fbe88cf6f9aadef9d8
deven-s <devendrasuryawanshi137@gmail.com>
2021-11-01 11:10:51 +0530

    Update RelativeLocators.py

--- RelativeLocators.py
+++ RelativeLocators.py
@@ -7,6 +7,6 @@ from selenium.webdriver.support.relative_locator import with_tag_name
 
 
 
-driver=webdriver.Chrome(executable_path=r"C:\Users\suryawanshi_de\Downloads\Chromedriver.exe")
+driver=webdriver.Chrome(executable_path=r"C:\Users\Downloads\Chromedriver.exe")
 driver.maximize_window()
 driver.get("https://opensource-demo.orangehrmlive.com/")
